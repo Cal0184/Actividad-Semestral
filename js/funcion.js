c53ba759eeae1d353ff1c3e9a98c96888ab1b8a6
Quezo_149 <134452519+Quezo149@users.noreply.github.com>
2024-05-13 18:11:04 -0400

    formulario

--- js/funcion.js
+++ js/funcion.js
@@ -1,6 +1,6 @@
 document.getElementById('formularioReg').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
-    var nombre = document.getElementById('nombre').value; // Captura el valor del campo de nombre
-    var email = document.getElementById('email').value; // Captura el valor del campo de email
-    console.log('Nombre:', nombre, 'Email:', email); // Muestra los datos en la consola
+    event.preventDefault();
+    var nombre = document.getElementById('nombre').value; 
+    var email = document.getElementById('email').value; 
+    console.log('Nombre:', nombre, 'Email:', email); 
 });
