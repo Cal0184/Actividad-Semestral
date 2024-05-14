e6b6238ab7d15db7c3b7c4aecf039a8ef2945d76
Quezo_149 <134452519+Quezo149@users.noreply.github.com>
2024-05-13 22:38:00 -0400

    Funciones registro

--- js/funcion.js
+++ js/funcion.js
@@ -1,6 +1,36 @@
 document.getElementById('formularioReg').addEventListener('submit', function(event) {
     event.preventDefault();
-    var nombre = document.getElementById('nombre').value; 
-    var email = document.getElementById('email').value; 
-    console.log('Nombre:', nombre, 'Email:', email); 
+
+    var nombre = document.getElementById('nombre').value;
+    var ape = document.getElementById('ape').value;
+    var email = document.getElementById('email').value;
+    var pass1 = document.getElementById('pass1').value;
+    var pass2 = document.getElementById('pass2').value;
+    var direccion = document.getElementById('direccion').value;
+    var ciudad = decument.getElementById('ciudad').value;
+    var region = document.getElementById('region').value;
+    var zip = document.getElementById('zip').velue;
+    
+    if (nombre === "" || ape === "" || email === "" || pass1 === "" || pass2 === "" || direccion === "" || ciudad === "" || zip === ""){
+        alert("Por favor, completa todos los campos.");
+    }else if (!validateEmail(email)){
+        alert("Por favor, ingresa un correo con formato válido.");
+    }else if (pass1!= pass2){
+        alert("Las contraseñas no coinciden.");
+    }else if (!validatePass(pass1)){
+        alert("La contraseña debe tener al menos 8 caracteres, incluyendo letras, números y al menos un carácter especial.");
+    }else {
+        console.log('Nombre:', nombre, 'Apellidos:', ape, 'Email:', email, 'Contraseña:', pass1, 'Dirección:', direccion, 'Ciudad:', ciudad, 'Región:', region, 'Zip:', zip);
+    }
 });
+
+function validateEmail(email) {
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+
+function validatePass(pass1) {
+    var regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return regex.test(pass1);
+}
+
