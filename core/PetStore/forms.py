6f02e75bc186a1bb9f0068c592d60e154ff90c3d
Quezo_149 <134452519+Quezo149@users.noreply.github.com>
2024-06-18 19:19:22 -0400

    ..

--- core/PetStore/forms.py
+++ core/PetStore/forms.py
@@ -1,54 +1,14 @@
-# forms.py
 from django import forms
-from django.forms import ModelForm
-from .models import Usuario
-import re
+from .models import Region, Usuario
 
-class formsRegistro(ModelForm):
-    REGIONES_CHILE =[
-        ('RM', 'Región Metropolitana de Santiago'),
-        ('1', 'Región de Tarapacá'),
-        ('2', 'Región de Antofagasta'),
-        ('3', 'Región de Atacama'),
-        ('4', 'Región de Coquimbo'),
-        ('5', 'Región de Valparaíso'),
-        ('6', 'Región del Libertador General Bernardo O\'Higgins'),
-        ('7', 'Región del Maule'),
-        ('8', 'Región del Biobío'),
-        ('9', 'Región de La Araucanía'),
-        ('10', 'Región de Los Lagos'),
-        ('11', 'Región de Aysén del General Carlos Ibáñez del Campo'),
-        ('12', 'Región de Magallanes y de la Antártica Chilena'),
-        ('14', 'Región de Los Ríos'),
-        ('15', 'Región de Arica y Parinacota'),
-        ('16', 'Región de Ñuble'),
-    ]
-    
-    nombre = forms.CharField(max_length=100, required=True)
-    apellidos = forms.CharField(max_length=100, required=True)
-    email = forms.EmailField(required=True)
-    _pass = forms.CharField(widget=forms.PasswordInput, required=True)
-    confirmar_pass = forms.CharField(widget=forms.PasswordInput, required=True)
-    direccion = forms.CharField(max_length=255, required=True)
-    region = forms.ChoiceField(choices=REGIONES_CHILE, required=True)
-    
+from django.forms import ModelForm
+
+class RegionForm(ModelForm):
+    class Meta:
+        model = Region
+        fields = ['id_region', 'nombre']
+        
+class UsuarioForm(ModelForm):
     class Meta:
         model = Usuario
         fields = "__all__"
-    
-    def clean_pass(self):
-        _pass = self.cleaned_data.get("_pass")
-        if len(_pass) < 8:
-            raise forms.ValidationError("La contraseña debe tener al menos 8 caracteres.")
-        if not re.search(r'\d', _pass):
-            raise forms.ValidationError("La contraseña debe contener al menos un número.")
-        return _pass
-    
-    def clean(self):
-        cleaned_data = super().clean()
-        _pass = cleaned_data.get("_pass")
-        confirmar_pass = cleaned_data.get("confirmar_pass")
-
-        if _pass and confirmar_pass:
-            if _pass != confirmar_pass:
-                raise forms.ValidationError("Las contraseñas no coinciden.")
--- core/PetStore/models.py
+++ core/PetStore/models.py
@@ -1,30 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Genero(models.Model):
-    id_genero = models.AutoField(db_column="idGenero", primary_key=True)
-    genero = models.CharField(max_length=20, blank=False, null=False)
-
+class Region(models.Model):
+    id_region = models.AutoField(primary_key=True, db_column="idRegion")
+    nombre = models.CharField(max_length=200, blank=False, null=False)
+    
     def __str__(self):
-        return str(self.genero)
+        return str(self.nombre)
     
 class Usuario(models.Model):
-    rut = models.CharField(primary_key=True , max_length=10)
-    nombre = models.CharField(max_length=20)
-    apellido_paterno = models.CharField(max_length=20)
-    apellido_materno = models.CharField(max_length=20)
-    fecha_nacimiento = models.DateField(blank=False, null=False)
-    id_genero = models.ForeignKey('Genero', on_delete=models.CASCADE, db_column='idGenero')
-    telefono = models.CharField(max_length=12)
-    email = models.EmailField(unique=True, max_length=100, blank=True, null=True)
-    direccion = models.CharField(max_length=50, blank=True, null=False)
-    activo = models.BooleanField()
-
-    def __str__(self) :
+    nombre = models.CharField(max_length=100, db_column="nombre")
+    apellidos = models.CharField(max_length=100, db_column="apellidos")
+    email = models.EmailField(unique=True, primary_key=True, db_column="email")
+    pass1 = models.CharField(max_length=128, db_column="contrasena")
+    direccion = models.CharField(max_length=255, db_column="direccion")
+    region = models.ForeignKey(Region, to_field='id_region', on_delete=models.SET_NULL, null=True, db_column="idRegion")
+    codZip = models.CharField(max_length=20, db_column="codZip")
+    
+    def __str__(self):
         return (
             str(self.nombre)
             + " "
-            +str(self.apellido_paterno)
-            +" "
-            +str(self.apellido_materno)
+            + str(self.apellidos)
         )
